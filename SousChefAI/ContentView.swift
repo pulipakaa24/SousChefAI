5daf0647044ef4a252fbefdeb7e5459fb1c284cb
pulipakaa24 <adipu@utexas.edu>
2026-02-11 11:43:56 -0600

    Initial Commit

---
+++ SousChefAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SousChefAI
+//
+//  Created by Aditya Pulipaka on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SousChefAI/SousChefAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SousChefAIApp.swift
+//  SousChefAI
+//
+//  Created by Aditya Pulipaka on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SousChefAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
